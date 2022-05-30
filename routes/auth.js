b6fba0459050d60a9412ffd882dce47542afcbfb
Satnam Sandhu <89329812+satnamSandhu2001@users.noreply.github.com>
2022-05-30 16:54:34 +0530

    certificate endpoint replaced with GET  from POST

--- routes/auth.js
+++ routes/auth.js
@@ -39,7 +39,7 @@ router.post(
   }
 );
 // get certificate using POST : " localhost:5000/api/auth/certificate "
-router.post(
+router.get(
   '/certificate',
   [
     body('serial', 'enter serial no. in numeric').isNumeric(),
