ee65651d8c488654af46768d5adc1764813c7782
Satnam Sandhu <89329812+satnamSandhu2001@users.noreply.github.com>
2022-05-30 19:43:49 +0530

    ports changed

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
-var host = server.address().address;
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://${host}${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 connectToMongo();
